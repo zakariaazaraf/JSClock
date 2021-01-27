5d4a5ba5292eb5e37b216367f183346b159e6a4e
zakaria ben salek <zakariaazaraf@gmail.com>
2021-01-27 18:51:22 +0100

    upgrade some stuff

--- js/script.js
+++ js/script.js
@@ -3,9 +3,25 @@ const numbers = document.querySelectorAll(".spans"),
     minute = document.querySelector(".minute"),
     hour = document.querySelector(".hour")
 
-let rotateValueSecond = 6,
-    rotateValueMinute = 6,
-    rotateValueHour = 3
+let dateNow = new Date(),
+    rotateValueSecond = 6 * dateNow.getSeconds(),
+    rotateValueMinute = 6 * dateNow.getMinutes(),
+    rotateValueHour
+if (dateNow.getHours() > 12) {
+    rotateValueHour = 30 * (dateNow.getHours() - 12)
+} else {
+    rotateValueHour = 30 * dateNow.getHours() + (dateNow.getMinutes() / 2)
+}
+
+second.style.transform = "translate(0, 11.11%) rotate(" + rotateValueSecond + "deg)"
+rotateValueSecond += 6;
+
+minute.style.transform = "translate(0, 25%) rotate(" + rotateValueMinute + "deg)"
+rotateValueMinute += 6;
+
+hour.style.transform = "translate(0, 42.85%) rotate(" + rotateValueHour + "deg)"
+rotateValueHour += 3;
+
 
 
 turnSecond = () => {
@@ -15,6 +31,14 @@ turnSecond = () => {
 
     second.style.transform = "translate(0, 11.11%) rotate(" + rotateValueSecond + "deg)"
     rotateValueSecond += 6;
+
+    if (rotateValueMinute > 360) {
+        rotateValueMinute = 0.1;
+    }
+
+    minute.style.transform = "translate(0, 25%) rotate(" + rotateValueMinute + "deg)"
+    rotateValueMinute += 0.1;
+
 
 }
 
@@ -41,7 +65,7 @@ turnHour = () => {
 
 
 let clockSecond = setInterval(turnSecond, 1000),
-    clockMinute = setInterval(turnMinute, 1000 * 60),
+    /* clockMinute = setInterval(turnMinute, 1000 * 60), */
     clockHour = setInterval(turnHour, 1000 * 300),
     increment = 30
 
